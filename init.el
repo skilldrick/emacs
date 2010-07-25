3933f6c9e9efbaf2ec8366de80e3e1e4789c7420
skilldrick <skilldrick@gmail.com>
2010-07-25 22:56:15 +0100

    Added .rjs support.

--- init.el
+++ init.el
@@ -44,6 +44,7 @@
 ;;; rhtml-mode
 (add-to-list 'load-path "~/.emacs.d/site-lisp/rhtml")
 (require 'rhtml-mode)
+(add-to-list 'auto-mode-alist '("\.rjs$" . rhtml-mode))
 (add-hook 'rhtml-mode-hook
      	  (lambda () (rinari-launch)))
 
