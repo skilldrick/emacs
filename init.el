2b0cea807baac511bfd1aacce86aeef758f09de6
skilldrick <skilldrick@gmail.com>
2010-09-06 13:14:48 +0100

    Commenting out passive mode makes ftp work on Windows.

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(ange-ftp-try-passive-mode t)
+;; '(ange-ftp-try-passive-mode t)
  '(dabbrev-upcase-means-case-search t)
  '(inhibit-startup-screen t))
 (custom-set-faces
