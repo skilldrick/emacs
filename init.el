5aaa960416813cd042392f3f85709f5cd4ad9f0e
Skilldrick <skilldrick@gmail.com>
2011-01-29 22:34:57 +0000

    Moved some color-themes about.

--- init.el
+++ init.el
@@ -4,7 +4,8 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(dabbrev-upcase-means-case-search t)
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(quack-fontify-style (quote emacs)))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
